19acbfe8a0caf5ea8a1783798049db0147916661
Georgii Brazhenko <68484505+Brajenko@users.noreply.github.com>
2024-12-16 18:47:26 +0000

    cmake fix

--- tests/test_end2end.cpp
+++ tests/test_end2end.cpp
@@ -27,25 +27,25 @@ bool equal_files(const std::string &a, const std::string &b) {
 }
 
 TEST_CASE("Test End 2 End") {
-    std::ifstream in("/home/gosha/projects/md-to-doc/test.md", std::ios_base::in);
+    std::ifstream in("/workspaces/md-to-doc/test.md", std::ios_base::in);
     md_parser::MdParser mparser;
     Document md = mparser.parse(in);
     in.close();
 
     nlohmann::json json = md.to_json();
-    std::ofstream out("/home/gosha/projects/md-to-doc/test.json", std::ios_base::out);
+    std::ofstream out("/workspaces/md-to-doc/test.json", std::ios_base::out);
     out << json.dump(4);
     out.close();
 
-    std::ifstream in2("/home/gosha/projects/md-to-doc/test.json", std::ios_base::in);
+    std::ifstream in2("/workspaces/md-to-doc/test.json", std::ios_base::in);
     json_parser::JSONParser json_parser;
     Document jd = json_parser.parse(in2);
     in2.close();
 
     json = jd.to_json();
-    std::ofstream out2("/home/gosha/projects/md-to-doc/test2.json", std::ios_base::out);
+    std::ofstream out2("/workspaces/md-to-doc/test2.json", std::ios_base::out);
     out2 << json.dump(4);
     out2.close();
 
-    REQUIRE(equal_files("/home/gosha/projects/md-to-doc/test.json", "/home/gosha/projects/md-to-doc/test2.json"));
+    REQUIRE(equal_files("/workspaces/md-to-doc/test.json", "/workspaces/md-to-doc/test2.json"));
 }
